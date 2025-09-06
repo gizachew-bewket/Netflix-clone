c2ec6181961852874718b1549aeb527b55c2899a
Gizachew Bewket <gizachewbewket19@gmail.com>
2025-09-06 10:31:29 +0300

    axios and request

--- Netflix-Clone/src/Components/Footer/Footer.jsx
+++ Netflix-Clone/src/Components/Footer/Footer.jsx
@@ -2,46 +2,59 @@ import React from "react";
 import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import YouTubeIcon from "@mui/icons-material/YouTube";
-import "./footer.css";
 
 const Footer = () => {
   return (
-    <footer className="outer-footer">
-      <div className="inner-footer-container">
+    <footer className=" text-white py-5">
+      <div className="container text-center">
         {/* Social Icons */}
-        <div className="footer-icons">
+        <div className="mb-4 d-flex justify-content-start gap-3">
           <FacebookOutlinedIcon />
           <InstagramIcon />
           <YouTubeIcon />
         </div>
 
-        {/* Links */}
-        <div className="footer-links">
-          <ul>
-            <li>Audio Description</li>
-            <li>Investor and Relations</li>
-            <li>Legal Notice</li>
-          </ul>
-          <ul>
-            <li>Help Center</li>
-            <li>Jobs</li>
-            <li>Cookie Preferences</li>
-          </ul>
-          <ul>
-            <li>Gift Cards</li>
-            <li>Terms of Use</li>
-            <li>Corporate Information</li>
-          </ul>
-          <ul>
-            <li>Media Center</li>
-            <li>Privacy</li>
-            <li>Contact Us</li>
-          </ul>
+        {/* Footer links */}
+        <div className="row text-start justify-content-center mb-4">
+          <div className="col-6 col-md-3 mb-1">
+            <ul className="list-unstyled">
+              <li>Audio Description</li>
+              <li>Investor and Relations</li>
+              <li>Legal Notice</li>
+            </ul>
+          </div>
+          <div className="col-6 col-md-3 mb-1">
+            <ul className="list-unstyled">
+              <li>Help Center</li>
+              <li>Jobs</li>
+              <li>Cookie Preferences</li>
+            </ul>
+          </div>
+          <div className="col-6 col-md-3 mb-1">
+            <ul className="list-unstyled">
+              <li>Gift Cards</li>
+              <li>Terms of Use</li>
+              <li>Corporate Information</li>
+            </ul>
+          </div>
+          <div className="col-6 col-md-3 mb-1">
+            <ul className="list-unstyled">
+              <li>Media Center</li>
+              <li>Privacy</li>
+              <li>Contact Us</li>
+            </ul>
+          </div>
         </div>
 
         {/* Service code and copyright */}
-        <div className="service-code">Service Code</div>
-        <div className="footer-text">© 1997-2024 Netflix, Inc.</div>
+        <div className="text-start">
+          <div className="mb-2 mt-0">
+            <button className="btn btn-outline-secondary btn-sm">
+              Service Code
+            </button>
+          </div>
+          <div>© 1997-2024 Netflix, Inc.</div>
+        </div>
       </div>
     </footer>
   );
