e507cdf09dbaa31c2bc0a3cb5fc95309e8ee28c0
Gizachew Bewket <gizachewbewket19@gmail.com>
2025-09-10 18:27:27 +0300

    final commit

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -1,9 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+// // import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()]
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()]
    
-});
+// });
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+export default defineConfig({
+  plugins: [react()],
+  base: "/Netflix-clone/",
+  build: {
+    outDir: "dist"
+  }})
